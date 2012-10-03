02ce2eb7ce8334dc579b819094b74e48c3f16b00
Nicholas Labich <nick@localhost.localdomain>
2012-10-03 19:09:08 -0400

    More progress on eval

--- src/main/scala/Environment.scala
+++ src/main/scala/Environment.scala
@@ -1,16 +1,21 @@
 package dvh.cek
 
-trait Environment extends Function1[Var, Closure] {
+trait Environment extends Function1[Symbol, Closure] {
   def domain: List[Var] = Nil
   def range: List[Closure] = Nil
+  def bind(v: Var, c: ValClosure): Environment
 }
 trait ListEnv extends Environment
 case object EmptyEnv extends ListEnv {
-  def apply(v: Var) =
+  def apply(n: Symbol) =
+    throw new RuntimeException("The variable " + n + " is not in this environment.")
+  def bind(v: Var, c: ValClosure) =
     throw new RuntimeException("The variable " + v.name + " is not in this environment.")
 }
 case class ConsEnv(v: Var, c: Closure, e: Environment) extends ListEnv {
-  def apply(v1: Var) = if (v.name == v1.name) c else e(v1)
+  def apply(n: Symbol) = if (v.name == n) c else e(n)
+  def bind(v1: Var, c1: ValClosure) =
+    if (v == v1) ConsEnv(v, c1, e) else ConsEnv(v, c, e.bind(v1, c1))
   override def domain = v :: e.domain
   override def range = c :: e.range
 }
--- src/main/scala/cek.scala
+++ src/main/scala/cek.scala
@@ -5,11 +5,19 @@ package object cek {
   def eval(ex: Expression): Value = {
     evalCek(ExpClosure(ex, EmptyEnv), EmptyKon)
   }
-  private def evalCek(c: Closure, k: Kontinuation): Value = c match {
-    case ExpClosure(App(m, n), e) =>
+  private def evalCek(c: Closure, k: Kontinuation): Value = (c, k) match {
+    case (ExpClosure(App(m, n), e), k) =>
       evalCek(ExpClosure(m, e), Ar(ExpClosure(n, e), k))
-    case ExpClosure(Oper(o, m::ms), e) =>
+    case (ExpClosure(Oper(o, m::ms), e), k) =>
       evalCek(ExpClosure(m, e), Op(o, Nil, ms map { n => ExpClosure(n, e) }, k))
+    case (ValClosure(Var(n), e), k) =>
+      evalCek(e(n), k)
+    case (v: ValClosure, Fn(ValClosure(Fun(x, m), e1), k1)) =>
+      evalCek(ExpClosure(m, e1.bind(x, v)), k)
+    case (v: ValClosure, Ar(ExpClosure(m, e1), k1)) =>
+      evalCek(ExpClosure(m, e1), Fn(v, k1))
+//    case (v: ValClosure, Op(o, vs, c::cs, k1)) =>
+      
   }
 
 }
